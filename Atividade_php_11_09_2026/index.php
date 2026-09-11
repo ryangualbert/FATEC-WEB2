988c7ac6f398664ea3b798d282d657173125953a
Ryan Gualberto <ryan13secondary@gmail.com>
2026-09-11 10:28:46 -0300

    [Adicionado] Aula: Codigo PHP.

--- Atividade_php_11_09_2026/index.php
+++ Atividade_php_11_09_2026/index.php
@@ -11,19 +11,38 @@
 
     <!-- Formulário para entrada dos dados -->
     <form method="POST">
+        <!-- Campo para informar o nome -->
+        <label>Nome:</label><br>
+        <input name="name" required>
 
         <!-- Campo para informar o e-mail -->
+        <br/>
+        <br/>
         <label>E-mail:</label><br>
         <input type="email" name="email" required>
+
+        <!-- Campo para informar o telefone -->
+        <br/>
+        <br/>
+        <label>Telefone:</label><br>
+        <input name="telefone" required>
 
         <br><br>
 
         <!-- Botão para enviar o formulário -->
         <button type="submit">Cadastrar</button>
-
     </form>
     <?php
     
+    // Verifica se o formulário foi enviado
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Recebe o e-mail enviado pelo formulário
+        $email = $POST["email"];
+
+        // Mostra o e-mail recebido
+        echo "E-mail recebido: " . $email;
+    }
+
     ?>
 </body>
 
